4ed471331093f0c1093835e95c6c275a79ecec2e
T.kawabata <52056772+momorara@users.noreply.github.com>
2025-04-21 09:29:58 +0900

    Update web_RemotePicoW.py

--- web_RemotePicoW.py
+++ web_RemotePicoW.py
@@ -82,6 +82,7 @@ def mqtt_broker_set(pin_code,mes):
     broker = 'mqtt.eclipseprojects.io' 
     mqtt_pub(broker,pin_code,mes)
     broker = 'broker.emqx.io'
+    print(broker,pin_code,mes)
     mqtt_pub(broker,pin_code,mes)
 
 def input():
