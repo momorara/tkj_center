2fd1b417479cc44ee05939f3b84623d4d41402ea
T.kawabata <52056772+momorara@users.noreply.github.com>
2025-04-24 13:56:27 +0900

    Update web_RemotePicoW.py

--- web_RemotePicoW.py
+++ web_RemotePicoW.py
@@ -32,8 +32,8 @@ import pytz
 # 設定値
 topic = "tkj/remote/2025/sw012345"      # mqttトピックス topic
 broker = "broker.hivemq.com"          # mqttブローカー
-henkan = "tmsgughinowcdgpjatzrefkrwx" # 暗号化コード たまに変えると良いかも 受信側にも同じコードが必要
-Web_title = 'WebRemote v21'
+henkan = "abcdefghijklmnopqrstuvwxyz" # 暗号化コード たまに変えると良いかも 受信側にも同じコードが必要
+Web_title = 'WebRemote v22'
 
 # スイッチの名称変更が可能です。
 sw_name0  = 'SW-0 @ RemotePico'
